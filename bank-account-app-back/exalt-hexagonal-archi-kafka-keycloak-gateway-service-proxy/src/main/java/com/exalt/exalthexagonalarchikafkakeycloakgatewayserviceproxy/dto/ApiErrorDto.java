e4496c2e7d56279434da5fe3ffb988be5db197cd
placidenduwayo <placide.nd@gmail.com>
2024-11-26 15:19:38 +0100

    feat(): deploy in docker gateway-proxy

--- bank-account-app-back/exalt-hexagonal-archi-kafka-keycloak-gateway-service-proxy/src/main/java/com/exalt/exalthexagonalarchikafkakeycloakgatewayserviceproxy/dto/ApiErrorDto.java
+++ bank-account-app-back/exalt-hexagonal-archi-kafka-keycloak-gateway-service-proxy/src/main/java/com/exalt/exalthexagonalarchikafkakeycloakgatewayserviceproxy/dto/ApiErrorDto.java
@@ -1,10 +1,12 @@
 package com.exalt.exalthexagonalarchikafkakeycloakgatewayserviceproxy.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Timestamp;
+
 @Builder
 @Setter
 @Getter
@@ -12,5 +14,6 @@ public class ApiErrorDto {
     private int errorCode;
     private String errorType;
     private String message;
+    @JsonFormat( shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd: hh:mm:ss")
     private Timestamp timestamp;
 }
