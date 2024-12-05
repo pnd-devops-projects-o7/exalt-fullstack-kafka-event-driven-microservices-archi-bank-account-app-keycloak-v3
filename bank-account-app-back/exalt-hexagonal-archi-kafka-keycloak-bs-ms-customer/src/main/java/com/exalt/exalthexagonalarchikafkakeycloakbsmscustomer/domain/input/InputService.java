e2521babd69d32207cc57c310b756ff875ac204b
Placide N <124048212+placidenduwayo1@users.noreply.github.com>
2024-12-05 18:11:33 +0100

    Feature/apis security (#3)
    * feat(): implement apis security
    
    * feat(): implement apis security
    
    * feat(): implement apis security
    
    * feat(customer-api): implement customer api security

--- bank-account-app-back/exalt-hexagonal-archi-kafka-keycloak-bs-ms-customer/src/main/java/com/exalt/exalthexagonalarchikafkakeycloakbsmscustomer/domain/input/InputService.java
+++ bank-account-app-back/exalt-hexagonal-archi-kafka-keycloak-bs-ms-customer/src/main/java/com/exalt/exalthexagonalarchikafkakeycloakbsmscustomer/domain/input/InputService.java
@@ -4,9 +4,11 @@ import com.exalt.exalthexagonalarchikafkakeycloakbsmscustomer.infra.output.dto.C
 import com.exalt.exalthexagonalarchikafkakeycloakbsmscustomer.infra.output.dto.CustomerResponseDto;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public interface InputService {
+    Map<String, Map<String,String>> getWelcome();
     CustomerResponseDto createCustomer(CustomerRequestDto customerRequestDto);
     List<CustomerResponseDto> loadAllPersistedCustomers();
     CustomerResponseDto getCustomerBy(String firstname, String lastname, String email);
